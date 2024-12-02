dca7a2779b4dc6b07d3c26ac7de14886ae888a61
Hanna Kebedom <hanna.kebedom@shopify.com>
2024-12-01 21:05:58 -0500

    part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,22 +1,47 @@
+from collections import defaultdict
+
 def part1():
     # read input file
-    a, b  = [], []
+    left, right  = [], []
     with open('day1/input.txt') as file:
         for line in file:
             nums = line.split()
-            a.append(int(nums[0]))
-            b.append(int(nums[1]))
+            left.append(int(nums[0]))
+            right.append(int(nums[1]))
 
     # sort both lists
-    a.sort()
-    b.sort()
+    left.sort()
+    right.sort()
 
     # calculate the difference between each element
     result = 0
-    for i in range(len(a)):
+    for i in range(len(left)):
         # sum the absolute difference between each element
-        result += abs(a[i] - b[i])
+        result += abs(left[i] - right[i])
 
     return result
 
-print(part1())
+def part2():
+    # read input file
+    left, right  = [], []
+    with open('day1/input.txt') as file:
+        for line in file:
+            nums = line.split()
+            left.append(int(nums[0]))
+            right.append(int(nums[1]))
+
+    # iterate through each number on the left
+    right_count = defaultdict(int)
+    result = 0
+    for num in left:
+        # count the number of times num appears in right
+        if num not in right_count:
+            right_count[num] = right.count(num)
+        result += num * right_count[num]
+    return result
+
+
+
+
+
+print(part2())
